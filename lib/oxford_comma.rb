d1c1d3afb5f9934589bd791989cb23138ca38cda
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-01-15 20:28:29 -0500

    commit tree fbb17a880ae099f0dbe72a4d1ffa4eca7562b4d3

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,29 @@
-def oxford_comma(array)
+ 
+ 
+ my_list = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"] 
 
+
+def oxford_comma(array)
+  
+    
+    if (array).length == 1
+        (array).join
+
+    elsif (array).length == 2
+        (array).join(" and ") 
+   
+    else
+        #(array).join(", ")
+        last_two=array[-2..]
+        remaining=array[0..-3].join(', ')
+
+        last_two_str=last_two.join(", and ")
+        return remaining + ', ' + last_two_str
+
+    end     
+                
 end
+
+oxford_comma(my_list)
+
+#eturn  oxford_comma(my_list)
